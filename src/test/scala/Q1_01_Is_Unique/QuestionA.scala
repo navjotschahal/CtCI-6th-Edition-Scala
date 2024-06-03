33caecdbdc0e0b4ed06e1d45d1a76a31c72a8491
nchahal2_uhg <navjot_chahal@optum.com>
2024-06-04 00:14:21 +0530

    (test): add unit test for Q1 01

--- src/test/scala/Q1_01_Is_Unique/QuestionA.scala
+++ src/test/scala/Q1_01_Is_Unique/QuestionA.scala
@@ -1,7 +1,18 @@
-class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
+// For more information on writing tests, see
+// https://scalameta.org/munit/docs/getting-started.html
+
+import Q1_01_Is_Unique.isUniqueChars
+
+class Q1_01_Is_Unique_Test_Suite extends munit.FunSuite {
+  test("test that string has unique chars") {
+    val obtained = isUniqueChars("abcde")
+    val expected = true
+    assertEquals(obtained, expected)
+  }
+
+  test("test that string does not have unique chars") {
+    val obtained = isUniqueChars("hello")
+    val expected = false
     assertEquals(obtained, expected)
   }
 }
